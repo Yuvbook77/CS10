8c480252c1d13be25c0f3ba450ecc7db25304519
Yuvbook77 <102550076+Yuvbook77@users.noreply.github.com>
2022-05-05 15:01:08 -0600

    Add files via upload

---
+++ cool2.java
@@ -0,0 +1,17 @@
+
+	public class cool2 {
+	public static void main(String[] args)  {
+
+	  System.out.println("|       |      ------\r\n"
+	  +" |     |       |\r\n"
+ 	  +"  |   |        ------\r\n"
+	  +"   | |              |\r\n"
+	  +"    |          ------");
+	  System.out.println("-------    ------\r\n"
+	  +"|          |\r\n"
+	  +"|          ------\r\n"
+	  +"|	        |\r\n"
+	  +"-------    ------");
+	}
+	  
+}
